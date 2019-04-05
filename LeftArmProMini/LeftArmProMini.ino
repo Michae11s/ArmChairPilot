7185bdb2818f0654c9c977689af4a45a050f215f
Michaels <47094517+Michae11s@users.noreply.github.com>
2019-04-05 14:40:06 -0400

    Updated to utilize debounce library instead of debounce function

--- LeftArmProMini/LeftArmProMini.ino
+++ LeftArmProMini/LeftArmProMini.ino
@@ -1,5 +1,6 @@
 #include <FIFO_Buff.h>
 #include <Wire.h>
+#include <Debounce.h>
 
 /*
  * Kyle Michaels
@@ -7,15 +8,6 @@
  * Written for ProMini 5V, Arduino 1.8.9
  * Sketch to pass current pin status to the i2c bus
  */
-
-#define PINMIN    2
-#define PINMAX    15
-#define db_delay  80
-#define mom_delay 350
-
-#define DOWN   1
-#define UP     2
-#define NOCNG  0
 
 //I2C Addressing
 #define ADDR   0x21
@@ -26,8 +18,9 @@
 
 FIFO_Buff<32> i2cBuff; //create a buffer object to hold the write cache
 
-bool btnState[PINMAX+1];
 bool sync = true;
+
+debounce<16> db;
 
 void setup()
 {
@@ -41,20 +34,20 @@ void setup()
    Wire.onReceive(receiveEvent);
 
   //Start pins in digital pull up
-   for (int i = PINMIN; i <= PINMAX; i++)
+   for (int i = 2; i <= 15; i++)
    {
       pinMode(i, INPUT_PULLUP);
-      btnState[i] = digitalRead(i);
+      dp.change(i);
    }
 }
 
 void loop()
 {
    //check all the button statuses
-   for(int i = PINMIN; i <= PINMAX; i++)
+   for(int i = 2; i <= 15; i++)
    {
-      uint8_t change = debounce(i);
-      if(change) //no change will be zero/false
+      uint8_t change = db.change(i);
+      if(change != NOCNG)
       {
          Serial.print("Pin updated: ");
          Serial.print(i);
@@ -64,7 +57,6 @@ void loop()
          //build the byte
          uint8_t device = ((ADDR & 0x0F) << 5) & D_MSK;
          uint8_t pin = (i << 1) & P_MSK;
-         change = change - 1;
          uint8_t packet = device | pin | change;
 
          if(i2cBuff.write(packet))
@@ -84,17 +76,16 @@ void requestEvent()
       i2cBuff.flush();
       for(int i = PINMIN; i <= PINMAX; i++)
       {
-         uint8_t state = btnState[i];
+         uint8_t status = db.status(i);
          Serial.print("Pin: ");
          Serial.print(i);
          Serial.print(" is: ");
-         etch(state);
+         etch(status);
 
          //build the byte
          uint8_t device = ((ADDR & 0x0F) << 5) & D_MSK;
          uint8_t pin = (i << 1) & P_MSK;
-         state = state;
-         uint8_t packet = device | pin | state;
+         uint8_t packet = device | pin | status;
 
          if(i2cBuff.write(packet))
             Serial.println("Buffer Full");
@@ -140,47 +131,10 @@ void receiveEvent(int Many)
 
 }
 
-uint8_t debounce(int pin)
+void etch(uint8_t status) //don't judge me I'm bad at names
 {
-   int t_delay;
-   switch (pin)
-   {
-   case 0xE:
-   case 0xF:
-      t_delay = mom_delay;
-      break;
-   default:
-      t_delay = db_delay;
-      break;
-   }
-   if(!digitalRead(pin))//is button down
-   {
-     if(btnState[pin]){ //button was up
-       delay(t_delay);
-       btnState[pin]=false;
-       return DOWN; //PIN DOWN
-     }
-     else
-      return NOCNG;//btn was down do nothing
-   }
-   else //button is up
-   {
-     if(btnState[pin]){//btn was up do nothing
-       return NOCNG;
-     }
-     else //btn was down lets debounce
-     {
-       delay(t_delay);
-       btnState[pin]=true;
-       return UP;
-     }
-   }
-}
-
-void etch(uint8_t state) //don't judge me I'm bad at names
-{
-   if (state == UP)
-      Serial.println("UP");
+   if (status == CLOSE)
+      Serial.println("CLOSE");
    else
-      Serial.println("DOWN");
+      Serial.println("OPEN");
 }
